b601b9a1df57b3536dade07e752f32a12187b247
张红妍 <zhanghongyan@zhanghongyandeMacBook-Pro.local>
2024-03-20 00:07:22 +0800

    3-19

---
+++ 3-19.h
@@ -0,0 +1,33 @@
+//
+// Created by 张红妍 on 2024/3/19.
+//
+
+#ifndef MYCODECPP_3_19_H
+#define MYCODECPP_3_19_H
+
+#endif //MYCODECPP_3_19_H
+#include <iostream>
+#include <climits>
+
+namespace space319 {
+    void FuncDigitSize() {
+        int n_int = INT_MAX;
+        short n_short = SHRT_MAX;
+        long n_long = LONG_MAX;
+        long long n_llong = LONG_LONG_MAX;
+
+        std::cout << "int is " << sizeof(n_int) << "bytes" << std::endl;
+        std::cout << "short is " << sizeof(n_short) << "bytes" << std::endl;
+        std::cout << "long is " << sizeof(n_long) << "bytes" << std::endl;
+        std::cout << "long long is " << sizeof(n_llong) << "bytes" << std::endl;
+
+        std::cout << "maximum values: " << std::endl;
+        std::cout << "int: " << n_int << std::endl;
+        std::cout << "short :" << n_short << std::endl;
+        std::cout << "long :" << n_long << std::endl;
+        std::cout << "long long " << n_llong << std::endl;
+
+        std::cout << "minimum" << INT_MIN << std::endl;
+        std::cout << "Bites per byte = " << CHAR_BIT << std::endl;
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include "3-12.h"
 #include "3-14.h"
+#include "3-19.h"
 int main() {
-    space312::FuncString();
+    /*space312::FuncString();
     space312::FuncArray();
     space312::FuncPoint();
 
-    space314::pointerFun();
+    space314::pointerFun();*/
+    space319::FuncDigitSize();
+
     return 0;
 }
