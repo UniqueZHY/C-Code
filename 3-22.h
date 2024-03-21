28439e08cff440fd5b9d2886c1eb543aad116809
张红妍 <zhanghongyan@zhanghongyandeMacBook-Pro.local>
2024-03-22 00:19:54 +0800

    3-22

---
+++ 3-22.h
@@ -0,0 +1,36 @@
+//
+// Created by 张红妍 on 2024/3/22.
+//
+
+#ifndef MYCODECPP_3_22_H
+#define MYCODECPP_3_22_H
+
+#endif //MYCODECPP_3_22_H
+#include <iostream>
+#include <climits>
+
+namespace space322 {
+    void func() {
+        short sam = SHRT_MAX;//32768
+        unsigned short sue = sam;//32768
+
+        std::cout << "sam has " << sam << " dollars and sue has " << sue << " dollars deposited." << std::endl;
+
+        //超出short最大值 故输出 -32768
+        sam += 1;
+        //32768
+        sue += 1;
+        std::cout << "sam has " << sam << " dollars and sue has " << sue << " dollars deposited." << std::endl;
+
+        sam = 0;
+        sue = 0;
+
+        // -1
+        sam -= 1;
+        //unsigned 不能为负数 故输出为 65535
+        sue -= 1;
+
+        std::cout << "sam has " << sam << " dollars and sue has " << sue << " dollars deposited." << std::endl;
+    }
+
+}
--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include "3-12.h"
 #include "3-14.h"
 #include "3-19.h"
+#include "3-22.h"
 int main() {
     /*space312::FuncString();
     space312::FuncArray();
     space312::FuncPoint();
 
-    space314::pointerFun();*/
-    space319::FuncDigitSize();
-
+    space314::pointerFun();
+    space319::FuncDigitSize();*/
+    space322::func();
     return 0;
 }
